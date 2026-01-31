ca5430358fcec867c9888350544aac919ccece29
Mario Kennedy Kavouras <mk@mariokennedy.com>
2026-01-31 15:21:10 -0800

    update logo

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
         className="h-[60vh] w-full max-w-3xl bg-center bg-no-repeat bg-contain flex flex-col items-center justify-center"
         aria-label="Father Figures logo"
       >
-        <Image src="/ff_logo_new.webp" alt="Father Figures logo" width={250} height={250} />
+        <Image src="/ff_logo.webp" alt="Father Figures logo" width={250} height={250} />
       </main>
     </div>
   );
